38e858bd9486d61778c73d7e5faedbe8d991d95a
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-01-26 18:21:36 +0500

    feat: add styles to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, CssBaseline, ThemeProvider} from '@material-ui/core';
+import {theme} from './theme';
+import './assets/style/main.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Title: React.FC = () => <h1>Hello</h1>;
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Container maxWidth="xl" style={{padding: 40}}>
+      <Title />
+    </Container>
+  </ThemeProvider>
+);
 
 export default App;
