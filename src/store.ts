94cfd718f33addb6cb68360f88c987a96cbf3761
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-02-03 17:49:04 +0500

    feat(store.ts): add types for store.ts

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,14 @@ import {postsMiddleware} from './postsMiddleware';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers =
-  process.env.NODE_ENV !== 'production' && typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  process.env.NODE_ENV !== 'production' &&
+  typeof (window as any) === 'object' &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 /* eslint-enable */
 
-const configureStore = (preloadedState) =>
+const configureStore = (preloadedState: any) =>
   createStore(
     rootReducer,
     preloadedState,
