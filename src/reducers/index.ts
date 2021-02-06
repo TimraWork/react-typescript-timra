cd7a173355780cf4bd4d4a6f7e63b220954e6ea8
Мухамеджанова Эльмира <timra.work@gmail.com>
2021-02-06 08:32:31 +0500

    feat(postsmiddleware.ts): add types for middleware

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,9 @@
 import {combineReducers} from 'redux';
 import posts from './posts';
 
-export default combineReducers({posts});
+const rootReducer = combineReducers({
+  posts
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,12 @@ import rootReducer from './reducers';
 import {save} from 'redux-localstorage-simple';
 import {postsMiddleware} from './postsMiddleware';
 
-/* eslint-disable no-underscore-dangle */
 const composeEnhancers =
   process.env.NODE_ENV !== 'production' &&
   typeof (window as any) === 'object' &&
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
-/* eslint-enable */
 
 const configureStore = (preloadedState: any) =>
   createStore(
